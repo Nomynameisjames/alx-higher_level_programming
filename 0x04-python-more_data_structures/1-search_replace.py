7a54b02b4da1ec2655786cd13dd467fcca6228c0
Nomynameisjames <adavaonimisi@gmail.com>
2022-09-01 17:18:57 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# function searches and replace in a list
+def search_replace(my_list, search, replace):
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
+
